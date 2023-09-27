d6f9205a9b8093815536b308d949e81536b2edb8
Andrew Beatty <andrew.beatty@atu.ie>
2023-09-27 14:10:46 +0100

    code changed in lecture

--- code/Topic02-datastructures/dr2.4-readfromcloud.py
+++ code/Topic02-datastructures/dr2.4-readfromcloud.py
@@ -2,3 +2,12 @@ import requests
 import json
 
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+response = requests.get(url)
+#print(response.json())
+data = response.json()
+with open ("bitcoindump.json", "w") as fp:
+    json.dump(data, fp)
+
+euroPriceObject = data["bpi"]["EUR"]
+rate = euroPriceObject["rate"]
+print(rate)
